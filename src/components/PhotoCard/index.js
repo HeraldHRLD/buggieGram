6e2845577aaf09bdfa75044a66b655c29bd913db
Gabi <g.federico.rodriguez@gmail.com>
2020-12-09 15:03:01 -0300

    hooks separated

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,57 +1,16 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react';
 import { Article, ImgWrapper, Img, Button } from './styles';
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { useNearScreen } from '../../hooks/useNearScreen';
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
 
-const useLocalStorage = (key, initialValue) => {
-  const [storedValue, setValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key)
-      return item !== null ? JSON.parse(item) : initialValue
-    } catch (e) {
-      return initialValue
-    }
-  })
-
-  const setLocalStorage = value => {
-    try {
-      window.localStorage.setItem(key, JSON.stringify(value))
-      setValue(value)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  return [storedValue, setLocalStorage]
-}
-
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
+  
+  const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  useEffect(() => {
-    import('intersection-observer')
-      .then(() => {
-        const observer = new window.IntersectionObserver((entries) => {
-          //console.log(entries)
-          const { isIntersecting } = entries[0]
-          if (isIntersecting) {
-            //console.log('si')
-            setShow(true)
-            observer.disconnect()
-          }
-        })
-        //console.log(element)
-        observer.observe(element.current)
-      })
-
-
-  }, [element])
-
-
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder
 
