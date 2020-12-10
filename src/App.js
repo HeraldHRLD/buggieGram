2f0781565631c8229d149cc059197a0ed9da4820
Gabi <g.federico.rodriguez@gmail.com>
2020-12-10 14:38:10 -0300

    Links ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React, { Fragment } from 'react'
-import { ListOfCategories } from './components/ListOfCategories'
+import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles';
-import { ListOfPhotoCards } from './container/ListOfPhotoCards';
 import Logo from './components/Logo/index';
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery';
+import { Detail } from './pages/Detail';
+import { Home } from './pages/Home';
+import { Router } from '@reach/router';
 
 const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-  
+
   return (
     <>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Fragment>
-            <ListOfCategories />
-            <ListOfPhotoCards  />
-          </Fragment>
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
+
 
     </>
   )
