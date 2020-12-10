9bcf06034e5fd77355d2501b45e5f02671117698
Gabi <g.federico.rodriguez@gmail.com>
2020-12-10 09:19:08 -0300

    graphql ready

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -7,7 +7,7 @@ import { useNearScreen } from '../../hooks/useNearScreen';
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  
+
   const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
